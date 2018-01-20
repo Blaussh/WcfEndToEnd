35b2f579cd847102894281e583ae34bbf68c295c
Blaussh <shai.gm@gmail.com>
2018-01-20 13:20:50 +0200

    using the MessageService in the client with the non-config approach

--- GeoLib/GeoLib.Client/MainWindow.xaml.cs
+++ GeoLib/GeoLib.Client/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
 using System.Threading;
 using System.Windows;
 using GeoLib.Client.Contracts;
@@ -57,8 +58,13 @@ namespace GeoLib.Client
 
         private void btnMakeCall_Click(object sender, RoutedEventArgs e)
         {
-            ChannelFactory<IMessageService> factory = new ChannelFactory<IMessageService>("");
+            EndpointAddress address = new EndpointAddress("net.tcp://localhost:8010/MessageService");
+            Binding binding = new NetTcpBinding();
+
+            //ChannelFactory<IMessageService> factory = new ChannelFactory<IMessageService>("");
+            ChannelFactory<IMessageService> factory = new ChannelFactory<IMessageService>(binding, address);
             IMessageService proxy = factory.CreateChannel();
+
             proxy.ShowMsg(txtMessage.Text);
             factory.Close();
         }
